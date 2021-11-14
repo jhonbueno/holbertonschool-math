0d3ecb2dd27230efcdc11846f38040638e1251aa
Jhon Bueno <jhon@monbu.co>
2021-11-14 08:58:46 +0000

    Update real and imaginary part

---
+++ 0x00-math_complex/8-complex.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * complex_from_mod_arg - singly linked list
+ * @c: complex type
+ *
+ *
+ *
+ * Return: none
+ */
+
+void complex_from_mod_arg (double m, double arg, complex *c3)
+{
+	c3->re = m;
+	c3->im = arg;
+}
