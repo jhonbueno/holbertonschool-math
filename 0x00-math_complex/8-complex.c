1869ab809f25d88fb67561a6fab32145deecf305
Jhon Bueno <jhon@monbu.co>
2021-11-14 22:11:05 +0000

    Update

--- 0x00-math_complex/8-complex.c
+++ 0x00-math_complex/8-complex.c
@@ -14,6 +14,6 @@
 
 void complex_from_mod_arg (double m, double arg, complex *c3)
 {
-	c3->re = m;
-	c3->im = arg;
+	c3->re = sqrt((m * m) / ((tan(arg) * tan(arg)) + 1));
+	c3->im = tan(arg) * c3->re;
 }
