88f5cc90fa7616d33bb8564cebc31d8f26fa4dd9
Jhon Bueno <jhon@monbu.co>
2021-11-14 07:41:00 +0000

    Addition

--- 0x00-math_complex/3-argument.c
+++ 0x00-math_complex/3-argument.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include "holberton.h"
-#include <math.h>
 
 /**
- * argument - modulus complex number
+ * addition - modulus complex number
  * @c: complex type
  *
  *
@@ -11,13 +10,13 @@
  * Return: double
  */
 
-double argument(complex c)
+void addition(complex c1, complex c2, complex *c3)
 {
-	complex *com;
-	double arg;
+	complex *com1, *com2;
 
-	com = &c;
-	arg = atan(com->im / com->re);
+	com1 = &c1;
+	com2 = &c2;
+	c3->re = com1->re + com2->re;
+	c3->im = com1->im + com2->im;
 
-	return (arg);
 }
