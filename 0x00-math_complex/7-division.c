0e54c84ca953b301b4ecf1c7b9c9d781f4f7ea30
Jhon Bueno <jhon@monbu.co>
2021-11-14 08:15:47 +0000

    Division

---
+++ 0x00-math_complex/7-division.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * division - modulus complex number
+ * @c: complex type
+ *
+ *
+ *
+ * Return: none
+ */
+
+void division(complex c1, complex c2, complex *c3)
+{
+	complex *com1, *com2;
+
+	com1 = &c1;
+	com2 = &c2;
+	c3->re = ((com1->re * com2->re) + (com1->im * com2->im))
+		/ ((com2->re * com2->re) + (com2->im * com2->im));
+	c3->im = (com1->im * com2->re) - (com1->re * com2->im)
+	        / ((com2->re * com2->re) + (com2->im * com2->im)); 
+
+}
